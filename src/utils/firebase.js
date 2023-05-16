1d0b658cd3c6257081ef3171d91424bb0875532c
Aref <devaref9@gmail.com>
2023-05-16 18:16:44 +0430

    Revert "removed .env vars."
    This reverts commit b2d60d87c44c36145b749f7f3077282305f8e6ce.

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  apiKey: "AIzaSyDp6QvL1txCZkPiCDE7UgYh5DVvwlKngvs",
 
   authDomain: "admin-panel-c664e.firebaseapp.com",
 
@@ -10,9 +10,9 @@ const firebaseConfig = {
 
   storageBucket: "admin-panel-c664e.appspot.com",
 
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  messagingSenderId: "1:954052223612:web:45a05fa2ad6e3d012e5ff9",
 
-  appId: process.env.REACT_APP_ID,
+  appId: "954052223612",
 };
 
 // Initialize Firebase
